c8c45f9f6c50bce6c57a5a89c31eab7cce90de20
Jeremy Seitz <jeremy@somebox.com>
2012-03-07 09:31:06 +0100

    update dragonfly config

--- config.ru
+++ config.ru
@@ -1,2 +1,18 @@
 require './logo_service'
+require 'dragonfly'
+
+Dragonfly[:images].configure_with(:imagemagick) do |c|
+  c.url_format = '/media/:job'
+  c.allow_fetch_url = true
+    c.log = Logger.new($stderr)
+    c.datastore.configure do |d|
+      d.root_path = './public/system'   # defaults to /var/tmp/dragonfly
+      d.server_root = './public'       # filesystem root for serving from - default to nil
+      d.store_meta = true              # default to true 
+    end
+end
+
+use Dragonfly::Middleware, :images
+
+
 run LogoService
--- logo_service.rb
+++ logo_service.rb
@@ -4,16 +4,7 @@ require 'dragonfly'
 require 'haml'
 
 class LogoService < Sinatra::Base
-  Dragonfly[:images].configure do |c|
-    c.url_format = '/media/:job'
-    c.log = Logger.new($stderr)
-    c.datastore.configure do |d|
-      d.root_path = '/Users/foz/Sites/logo_service/public/system'   # defaults to /var/tmp/dragonfly
-      d.server_root = '/Users/foz/Sites/logo_service/public'       # filesystem root for serving from - default to nil
-      d.store_meta = true              # default to true 
-    end
-  end
-  
+
   get '/' do
     'logo service'
   end
@@ -23,7 +14,7 @@ class LogoService < Sinatra::Base
                     :what=>params[:what], 
                     :where=>params[:where],
                     :ads=>true,
-                    :limit=>20,
+                    :limit=>200,
                     :cluster=>true,
                     :api_slot=>'yellow'
                   )
@@ -32,8 +23,5 @@ class LogoService < Sinatra::Base
     haml :search
   end
   
-  get '/logo/:id' do |size, format|
-    app.fetch_file('~/some/image.png').thumb(size).encode(format).to_response(env)
-  end
 
 end
